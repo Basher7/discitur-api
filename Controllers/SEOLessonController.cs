f1640c4ff62fbbb1467e40ca4fe2299d5acb5fe0
William Verdolini <william.verdolini@gmail.com>
2014-04-22 14:27:57 +0200

    Rewrite URL for SEO Ajax Crawling and MVC5 views

---
+++ Controllers/SEOLessonController.cs
@@ -0,0 +1,41 @@
+﻿using Mag14.discitur.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Mag14.Controllers
+{
+    public class SEOLessonController : Controller
+    {
+        private DisciturContext db = new DisciturContext();
+
+        public string Index()
+        {
+            return "This is my <b>default</b> action...";
+        }
+
+        public ActionResult List()
+        {
+            return View(db.Lessons.ToList());
+        }
+
+        public async Task<ActionResult> Detail(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Lesson lesson = await db.Lessons.FindAsync(id);
+            if (lesson == null)
+            {
+                return HttpNotFound();
+            }
+            return View(lesson);
+        }
+
+	}
+}
