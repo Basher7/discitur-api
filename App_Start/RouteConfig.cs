21078da4bead6e34fbf2900d3bd95797a39624e0
William Verdolini <william.verdolini@gmail.com>
2014-05-08 13:59:43 +0200

    MVC Routing ignored for root path (index.html)

--- App_Start/RouteConfig.cs
+++ App_Start/RouteConfig.cs
@@ -12,6 +12,7 @@ namespace AngulaDemo
         public static void RegisterRoutes(RouteCollection routes)
         {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+            routes.IgnoreRoute("");
 
             routes.MapRoute(
                 name: "Default",
